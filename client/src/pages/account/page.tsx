c965ffd662b9b5c2000de3f433f23075a5f47488
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-08-22 15:41:31 +0100

    created basic account page for the time being

--- client/src/pages/account/page.tsx
+++ client/src/pages/account/page.tsx
@@ -1,5 +1,14 @@
+import Card from '@/components/ui/cards/Card';
+import useProfile from '@/components/auth/useProfile';
+
 export default function AccountPage() {
+  const { data } = useProfile();
+
   return (
-    <div>AccountPage</div>
+    <div className='h-full flex flex-col items-center justify-center py-10 mx-4'>
+      <Card className='break-all'>
+        {JSON.stringify(data)}
+      </Card>
+    </div>
   );
 }
