4523d7d4ca036f63089bd33eb6541e65429f3529
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-08-31 19:59:22 +0100

    added all tests for win-loss feature on server

--- server/src/features/win-loss/win-loss.test.ts
+++ server/src/features/win-loss/win-loss.test.ts
@@ -1,49 +1,22 @@
 /* eslint-env jest */
-// eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
 // TODO change dotenv so that it loads different env files for prod, dev and test
 import '../../config/dotenv';
-// import { jest } from '@jest/globals';
 import app from '../../app';
-import * as winLossServices from './win-loss.services';
 import * as authServices from '../auth/auth.services';
 import config from '../../config/config';
 import { prismaMock } from '../../singleton';
 
 describe('win-loss', () => {
-  describe('ROUTE: GET /winLoss', () => {
-    describe('given the user is not logged in', () => {
-      it('should return a 401', async () => {
+  describe('GET /winLoss', () => {
+    describe('given that authentication fails', () => {
+      it('should return status 401', async () => {
         await request(app).get('/api/v1/winLoss').expect(401);
       });
     });
 
-    describe('given the user is logged in', () => {
-      // it('should return a status 200 and the wins and losses', async () => {
-      //   const userId = 1;
-      //   const accessToken = authServices.generateAccessToken(userId);
-      //   const cookie = `${config.ACCESS_TOKEN_COOKIE_NAME}=${accessToken}`;
-
-      //   const getUserServiceMock = jest
-      //     .spyOn(authServices, 'getUser')
-      //     // eslint-disable-next-line max-len
-      //     .mockReturnValueOnce(Promise.resolve({ userId: 1, email: null, image: null, name: null }));
-      //   const getWinsLossServiceMock = jest
-      //     .spyOn(winLossServices, 'getWinLoss')
-      //     .mockReturnValueOnce(Promise.resolve({ wins: 0, losses: 0 }));
-
-      //   const { body, statusCode } = await request(app)
-      //     .get('/api/v1/winLoss')
-      //     .set('Cookie', [cookie]);
-
-      //   expect(getUserServiceMock).toHaveBeenCalledWith(userId);
-      //   expect(getWinsLossServiceMock).toHaveBeenCalledWith(userId);
-      //   expect(statusCode).toBe(200);
-      //   expect(body).toEqual({ wins: 0, losses: 0 });
-      //   getWinsLossServiceMock.mockRestore();
-      // });
-
-      it('should return a status 200 and the wins and losses', async () => {
+    describe('given that authentication succeeds', () => {
+      it('should return status 200 and body with wins and losses', async () => {
         const winsLosses = { wins: 0, losses: 0 };
         const userId = 1;
 
@@ -58,6 +31,92 @@ describe('win-loss', () => {
           .set('Cookie', [cookie]);
 
         expect(prismaMock.user.findUnique).toHaveBeenCalledTimes(2);
+        expect(statusCode).toBe(200);
+        expect(body).toEqual(winsLosses);
+      });
+    });
+  });
+
+  describe('POST /wins/increment', () => {
+    describe('given that the body is invalid', () => {
+      it('should return status 400', async () => {
+        await request(app)
+          .post('/api/v1/wins/increment')
+          .send({ wins: -5 })
+          .expect(400);
+      });
+    });
+
+    describe('given that the body is valid but authentication fails', () => {
+      it('should return status 401', async () => {
+        await request(app)
+          .post('/api/v1/wins/increment')
+          .send({ wins: 3 })
+          .expect(401);
+      });
+    });
+
+    describe('given that the body is valid and authentication succeeds', () => {
+      it('should return status 200 and body with new wins and losses', async () => {
+        const userId = 1;
+        const incrementWins = 3;
+        const winsLosses = { wins: incrementWins, losses: 0 };
+        const accessToken = authServices.generateAccessToken(userId);
+        const cookie = `${config.ACCESS_TOKEN_COOKIE_NAME}=${accessToken}`;
+
+        prismaMock.user.findUnique.mockResolvedValueOnce({ userId } as any);
+        prismaMock.user.update.mockResolvedValueOnce(winsLosses as any);
+
+        const { body, statusCode } = await request(app)
+          .post('/api/v1/wins/increment')
+          .send({ wins: incrementWins })
+          .set('Cookie', [cookie]);
+
+        expect(prismaMock.user.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.user.update).toHaveBeenCalledTimes(1);
+        expect(statusCode).toBe(200);
+        expect(body).toEqual(winsLosses);
+      });
+    });
+  });
+
+  describe('POST /losses/increment', () => {
+    describe('given that the body is invalid', () => {
+      it('should return status 400', async () => {
+        await request(app)
+          .post('/api/v1/losses/increment')
+          .send({ losses: -5 })
+          .expect(400);
+      });
+    });
+
+    describe('given that the body is valid but authentication fails', () => {
+      it('should return status 401', async () => {
+        await request(app)
+          .post('/api/v1/losses/increment')
+          .send({ losses: 3 })
+          .expect(401);
+      });
+    });
+
+    describe('given that the body is valid and authentication succeeds', () => {
+      it('should return status 200 and body with new wins and losses', async () => {
+        const userId = 1;
+        const incrementLosses = 3;
+        const winsLosses = { wins: 0, losses: incrementLosses };
+        const accessToken = authServices.generateAccessToken(userId);
+        const cookie = `${config.ACCESS_TOKEN_COOKIE_NAME}=${accessToken}`;
+
+        prismaMock.user.findUnique.mockResolvedValueOnce({ userId } as any);
+        prismaMock.user.update.mockResolvedValueOnce(winsLosses as any);
+
+        const { body, statusCode } = await request(app)
+          .post('/api/v1/losses/increment')
+          .send({ losses: incrementLosses })
+          .set('Cookie', [cookie]);
+
+        expect(prismaMock.user.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.user.update).toHaveBeenCalledTimes(1);
         expect(statusCode).toBe(200);
         expect(body).toEqual(winsLosses);
       });
