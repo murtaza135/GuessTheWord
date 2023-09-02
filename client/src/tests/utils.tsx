d05e8e365045d903c1c9c9e2d841ecf5abb3487f
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-09-02 17:44:40 +0100

    added tests for register

--- client/src/tests/utils.tsx
+++ client/src/tests/utils.tsx
@@ -1,18 +1,64 @@
 import { render } from '@testing-library/react';
 import { rest } from 'msw';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, createRoutesFromElements, createMemoryRouter, RouterProvider } from 'react-router-dom';
 
 export const handlers = [
   rest.post(
     '*/auth/login/local',
-    (req, res, ctx) => {
+    (_req, res, ctx) => {
       return res(
         ctx.status(204),
-        ctx.cookie("access", "test")
+        ctx.cookie("access", "1")
       );
     }
-  )
+  ),
+  rest.post(
+    '*/auth/register/local',
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(204),
+        ctx.cookie("access", "1")
+      );
+    }
+  ),
+  rest.post(
+    '*/auth/authorize/local',
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(204),
+      );
+    }
+  ),
+  rest.get(
+    '*/auth/profile',
+    (_req, res, ctx) => {
+      const user = { userId: 1 };
+      return res(
+        ctx.status(200),
+        ctx.json(user)
+      );
+    }
+  ),
+  rest.get(
+    '*/auth/accounts',
+    (_req, res, ctx) => {
+      const accounts = { localAccount: null, oAuthAccounts: [] };
+      return res(
+        ctx.status(200),
+        ctx.json(accounts)
+      );
+    }
+  ),
+  rest.post(
+    '*/auth/logout',
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(204),
+        ctx.cookie("access", "")
+      );
+    }
+  ),
 ];
 
 const createTestQueryClient = () => new QueryClient({
@@ -28,23 +74,25 @@ const createTestQueryClient = () => new QueryClient({
   }
 });
 
-export function renderWithClient(ui: React.ReactElement, initialRoute?: string) {
+export function renderWithClient(ui: React.ReactElement) {
   const testQueryClient = createTestQueryClient();
+
   const { rerender, ...result } = render(
     <QueryClientProvider client={testQueryClient}>
-      <MemoryRouter initialEntries={[initialRoute ?? '/']}>
+      <MemoryRouter>
         <Routes>
           <Route path="/" element={ui} />
         </Routes>
       </MemoryRouter>
     </QueryClientProvider>
   );
+
   return {
     ...result,
     rerender: (rerenderUi: React.ReactElement) =>
       rerender(
         <QueryClientProvider client={testQueryClient}>
-          <MemoryRouter initialEntries={[initialRoute ?? '/']}>
+          <MemoryRouter>
             <Routes>
               <Route path="/" element={rerenderUi} />
             </Routes>
@@ -54,23 +102,25 @@ export function renderWithClient(ui: React.ReactElement, initialRoute?: string) 
   };
 }
 
-export function renderWithClientWithRoutes(routes: React.ReactNode, initialRoute?: string) {
+export function renderWithClientAndRouter(routes: React.ReactNode, config?: { initialRoute?: string; }) {
   const testQueryClient = createTestQueryClient();
+
   const { rerender, ...result } = render(
     <QueryClientProvider client={testQueryClient}>
-      <MemoryRouter initialEntries={[initialRoute ?? '/']}>
+      <MemoryRouter initialEntries={[config?.initialRoute ?? '/']}>
         <Routes>
           {routes}
         </Routes>
       </MemoryRouter>
     </QueryClientProvider>
   );
+
   return {
     ...result,
     rerender: (rerenderRoutes: React.ReactNode) =>
       rerender(
         <QueryClientProvider client={testQueryClient}>
-          <MemoryRouter initialEntries={[initialRoute ?? '/']}>
+          <MemoryRouter initialEntries={[config?.initialRoute ?? '/']}>
             <Routes>
               {rerenderRoutes}
             </Routes>
@@ -80,11 +130,39 @@ export function renderWithClientWithRoutes(routes: React.ReactNode, initialRoute
   };
 }
 
-export function createWrapper(initialRoute?: string) {
+export function renderWithClientAndRouter2(routes: React.ReactNode, config?: { initialRoute?: string; }) {
   const testQueryClient = createTestQueryClient();
+
+  const router = createMemoryRouter(
+    createRoutesFromElements(routes),
+    { initialEntries: [config?.initialRoute ?? '/'] },
+  );
+
+  const { rerender, ...result } = render(
+    <QueryClientProvider client={testQueryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+
+  return {
+    ...result,
+    rerender: (rerenderRoutes: React.ReactNode) => {
+      const newRouter = createMemoryRouter(createRoutesFromElements(rerenderRoutes), { initialEntries: [config?.initialRoute ?? '/'] });
+      return rerender(
+        <QueryClientProvider client={testQueryClient}>
+          <RouterProvider router={newRouter} />
+        </QueryClientProvider>
+      );
+    },
+  };
+}
+
+export function createWrapper(config?: { initialRoute?: string; }) {
+  const testQueryClient = createTestQueryClient();
+
   return ({ children }: { children: React.ReactNode; }) => (
     <QueryClientProvider client={testQueryClient}>
-      <MemoryRouter initialEntries={[initialRoute ?? '/']}>
+      <MemoryRouter initialEntries={[config?.initialRoute ?? '/']}>
         <Routes>
           <Route path="/" element={children} />
         </Routes>
