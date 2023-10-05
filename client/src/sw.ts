9a7dc1435c4bb18cfd088ef16b1798644680712c
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-05 23:22:57 +0100

    added offline support for increment wins in sw.ts

--- client/src/sw.ts
+++ client/src/sw.ts
@@ -6,6 +6,10 @@ import { registerRoute } from 'workbox-routing';
 // import { NetworkOnly } from 'workbox-strategies';
 import config from './config/config';
 // import { queryClient } from './app/api/queryClient';
+import { WinLossResponse } from './features/win-loss/types';
+
+type WinResponse = Pick<WinLossResponse, 'wins'>;
+type LossResponse = Pick<WinLossResponse, 'losses'>;
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -17,7 +21,8 @@ cleanupOutdatedCaches();
 self.skipWaiting();
 clientsClaim();
 
-const queue = new Queue('guess-the-word-increment-losses');
+const winsQueue = new Queue('guess-the-word-increment-wins');
+const lossesQueue = new Queue('guess-the-word-increment-losses');
 
 self.addEventListener('sync', () => {
   setTimeout(() => {
@@ -32,22 +37,46 @@ self.addEventListener('sync', () => {
 registerRoute(
   `${config.VITE_API_URL}/losses/increment`,
   async ({ request }) => {
-    console.log("FETCH 1");
     try {
       const response = await fetch(request.clone());
-      console.log(response);
       return response;
     } catch (error) {
-      console.log("HERE");
-      const req = await queue.popRequest();
-      const numLosses = (await req?.request.json())?.losses ?? 0;
-      const newReq = new Request(request, { body: JSON.stringify({ losses: numLosses + 1 }) });
-      await queue.pushRequest({ request: newReq });
-      // return error as Response; // TODO
-      // return true as unknown as Response;
-      const res = new Response();
-      return res;
+      console.log(error);
+      const prevReq = await lossesQueue.popRequest();
+      const prevData = await prevReq?.request?.json() as LossResponse | undefined;
+      const prevLosses = prevData?.losses ?? 0;
+      const newData = await request.json() as LossResponse;
+      const newLosses = newData.losses;
+      const newReq = new Request(request, {
+        body: JSON.stringify({ losses: prevLosses + newLosses }),
+      });
+      await lossesQueue.pushRequest({ request: newReq });
+      return new Response();
     }
   },
   'POST'
 );
+
+registerRoute(
+  `${config.VITE_API_URL}/wins/increment`,
+  async ({ request }) => {
+    try {
+      const response = await fetch(request.clone());
+      return response;
+    } catch (error) {
+      console.log(error);
+      const prevReq = await winsQueue.popRequest();
+      const prevData = await prevReq?.request?.json() as WinResponse | undefined;
+      const prevWins = prevData?.wins ?? 0;
+      const newData = await request.json() as WinResponse;
+      const newWins = newData.wins;
+      const newReq = new Request(request, {
+        body: JSON.stringify({ wins: prevWins + newWins }),
+      });
+      await winsQueue.pushRequest({ request: newReq });
+      return new Response();
+    }
+  },
+  'POST'
+);
+
