62db7364aac0649071ad646fe67e94a0318d16d1
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-06 19:35:52 +0100

    added comments to sw.ts

--- client/src/sw.ts
+++ client/src/sw.ts
@@ -18,20 +18,39 @@ cleanupOutdatedCaches();
 self.skipWaiting();
 clientsClaim();
 
+// queues to hold failed requests
 const winsQueue = new Queue('guess-the-word-increment-wins');
 const lossesQueue = new Queue('guess-the-word-increment-losses');
 
-self.addEventListener('sync', () => {
-  console.log("SYNC");
-  setTimeout(() => {
-    self.clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ refetch: true });
+// on failed wins increment due to being offline,
+// add request to queue to be retried on reconnect/sync
+registerRoute(
+  `${config.VITE_API_URL}/wins/increment`,
+  async ({ request }) => {
+    try {
+      const response = await fetch(request.clone());
+      return response;
+    } catch (error) {
+      // instead of sending multiple requests to increment wins 1 by 1,
+      // combine number of wins to increment from all requests and push
+      // a single request back into the queue
+      const prevReq = await winsQueue.popRequest();
+      const prevData = await prevReq?.request?.json() as WinResponse | undefined;
+      const prevWins = prevData?.wins ?? 0;
+      const newData = await request.json() as WinResponse;
+      const newWins = newData.wins;
+      const newReq = new Request(request, {
+        body: JSON.stringify({ wins: prevWins + newWins }),
       });
-    });
-  }, 500);
-});
+      await winsQueue.pushRequest({ request: newReq });
+      return new Response();
+    }
+  },
+  'POST'
+);
 
+// on failed losses increment due to being offline,
+// add request to queue to be retried on reconnect/sync
 registerRoute(
   `${config.VITE_API_URL}/losses/increment`,
   async ({ request }) => {
@@ -39,7 +58,9 @@ registerRoute(
       const response = await fetch(request.clone());
       return response;
     } catch (error) {
-      console.log(error);
+      // instead of sending multiple requests to increment losses 1 by 1,
+      // combine number of losses to increment from all requests and push
+      // a single request back into the queue
       const prevReq = await lossesQueue.popRequest();
       const prevData = await prevReq?.request?.json() as LossResponse | undefined;
       const prevLosses = prevData?.losses ?? 0;
@@ -56,25 +77,15 @@ registerRoute(
   'POST'
 );
 
-registerRoute(
-  `${config.VITE_API_URL}/wins/increment`,
-  async ({ request }) => {
-    try {
-      const response = await fetch(request.clone());
-      return response;
-    } catch (error) {
-      console.log(error);
-      const prevReq = await winsQueue.popRequest();
-      const prevData = await prevReq?.request?.json() as WinResponse | undefined;
-      const prevWins = prevData?.wins ?? 0;
-      const newData = await request.json() as WinResponse;
-      const newWins = newData.wins;
-      const newReq = new Request(request, {
-        body: JSON.stringify({ wins: prevWins + newWins }),
+// send message to browser to refetch data again (via react query)
+// on reconnect/sync
+self.addEventListener('sync', () => {
+  console.log("SYNC");
+  setTimeout(() => {
+    self.clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage({ refetch: true });
       });
-      await winsQueue.pushRequest({ request: newReq });
-      return new Response();
-    }
-  },
-  'POST'
-);
+    });
+  }, 500);
+});
