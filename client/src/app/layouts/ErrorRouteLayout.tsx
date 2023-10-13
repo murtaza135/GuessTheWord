d03c751bea3b9a7c2796fce3fc3e614eccc09683
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-13 18:02:29 +0100

    made error page more robust

--- client/src/app/layouts/ErrorRouteLayout.tsx
+++ client/src/app/layouts/ErrorRouteLayout.tsx
@@ -1,10 +1,29 @@
 import Container from '@/ui/containers/Container';
-import ErrorPage from '@/pages/error/page';
+import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse } from 'react-router-dom';
+import Button from '@/ui/buttons/Button';
+import Card from '@/ui/cards/Card';
+import { Link } from 'react-router-dom';
+import Title from '@/ui/Title';
 
 export default function ErrorRouteLayout() {
+  const error = useRouteError();
+  const isPageNotFoundError = isRouteErrorResponse(error) && error.status === 404;
+
   return (
     <Container $variant='center' className='px-4 py-24'>
-      <ErrorPage />
+      <Title title={isPageNotFoundError ? '404' : 'Error'} />
+      <Card className='flex flex-col gap-10 items-center'>
+        <div className='flex flex-col gap-3'>
+          <p className='text-center text-primary-900 font-bold text-5xl md:text-5xl'>
+            {isPageNotFoundError ? '404' : 'Error'}
+          </p>
+          <p className='text-center text-primary-900 font-bold text-2xl md:text-2xl'>
+            {isPageNotFoundError ? 'Page not found' : 'Something went wrong'}
+          </p>
+        </div>
+        <Link to="/"><Button>Main Menu</Button></Link>
+      </Card>
     </Container>
   );
 }
