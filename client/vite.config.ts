16388b3ebc5f2b0beb22384e38c5efc38d13d0ac
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-13 16:45:10 +0100

    cleaned up code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,4 @@
-// import { defineConfig } from 'vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
@@ -55,11 +54,6 @@ export default defineConfig({
   build: {
     outDir: './dist',
     emptyOutDir: true
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
   },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }]
