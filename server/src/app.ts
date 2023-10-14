cbd1746fcb9054647972b3213c87468fb572d13e
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-14 18:50:49 +0100

    testing deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,7 +37,7 @@ router.use(cookieSession({
   name: config.SESSION_COOKIE_NAME,
   keys: [config.COOKIE_SECRET],
   httpOnly: true,
-  sameSite: 'none',
+  sameSite: false,
   secure: true,
   maxAge: config.SESSION_COOKIE_MAX_AGE,
 }));
