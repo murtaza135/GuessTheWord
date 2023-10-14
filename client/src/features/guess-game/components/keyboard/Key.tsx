1fb11c548f2fd9bb600337e88f97113352648c7b
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2023-10-14 16:52:25 +0100

    minor changes

--- client/src/features/guess-game/components/keyboard/Key.tsx
+++ client/src/features/guess-game/components/keyboard/Key.tsx
@@ -8,7 +8,7 @@ type Props = Omit<React.ComponentProps<"button">, 'onClick'> & {
 export function Key({ value, onClick, ...rest }: Props) {
   return (
     <button
-      className='bg-primary-900 text-white px-2.5 py-1 md:px-4 md:py-2 rounded-md hover:opacity-75 transition-opacity md:text-lg font-semibold'
+      className='bg-primary-900 text-white px-3.5 py-1.5 md:px-4 md:py-2 rounded-md hover:opacity-75 transition-opacity md:text-lg font-semibold'
       onClick={(event) => onClick?.(value, event)}
       {...rest}
     >
